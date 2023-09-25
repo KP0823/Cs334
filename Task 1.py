88fd5aadb46fcf490267231d7102a3bd8e3a771e
Kp0823 <kushparmar0823@gmail.com>
2023-09-25 01:36:38 -0400

    Startings of this assignment

---
+++ Task 1.py
@@ -0,0 +1,40 @@
+
+import sys,itertools
+
+global states,sigma, dlt, qs,F
+
+def read_from_stdin():
+    global states,sigma, dlt, qs,F
+
+    states = input()
+    states= eval(states)
+
+    sigma = sys.stdin.readline()
+    sigma = eval(sigma)
+    
+    dlt = sys.stdin.readline()
+    dlt = eval(dlt)
+
+    qs = sys.stdin.readline()
+    qs = eval(qs)
+
+    F = sys.stdin.readline()
+    F = eval(F)
+
+    return states, sigma, dlt, qs, F
+
+def NFA():
+   global states,sigma, dlt, qs,F
+   dic ={}
+   keys=list(itertools.product(states,sigma))
+   for i in keys:
+       dic[i]=[]
+   
+
+
+def main():
+    read_from_stdin()
+    NFA()
+
+if __name__=="__main__":
+    main()
