d0d4055fde70a047315d2e2f20cf544f0c240138
KushParmar <kushparmar0823@gmail.com>
2023-09-25 11:08:18 -0400

    Finish Task 1

--- Task 1.py
+++ Task 1.py
@@ -1,5 +1,5 @@
 
-import sys,itertools
+import sys,itertools,pprint
 
 global states,sigma, dlt, qs,F
 
@@ -24,17 +24,30 @@ def read_from_stdin():
     return states, sigma, dlt, qs, F
 
 def NFA():
-   global states,sigma, dlt, qs,F
-   dic ={}
-   keys=list(itertools.product(states,sigma))
-   for i in keys:
-       dic[i]=[0]
-   
+    global states,sigma, dlt, qs,F
+    dic ={}
+    keys=list(itertools.product(states,sigma))
+    for i in keys:
+        dic[i]=[]
+    for key, value in dlt.items():
+        if key[1]=='E':
+            dic[(key[0],'0')]+=[value]
+            dic[(key[0],'1')]+=[value]
 
+        else:
+            dic[key]+=[value]
+    return dic
 
 def main():
     read_from_stdin()
-    NFA()
+    result=NFA()
+    last_key=list(result)[-1]
+    for key,value in result.items():
+        if(key!=last_key):
+            print(str(key) +": "+str(value)+",")
+        else:
+             print(str(key) +": "+str(value)+"")
+    
 
 if __name__=="__main__":
     main()
